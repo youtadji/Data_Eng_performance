043cfcd8290f39e71905d4e27559c9c4de5015f7
Eya Aydi <eyaaydi999@gmail.com>
2026-04-16 16:33:41 +0200

    cleanup2 removing the strings ones alsoo again

--- src/com/tu/berlin/thesis/operators/Operator.java
+++
@@ -1,7 +0,0 @@
-package com.tu.berlin.thesis.operators;
-
-public interface Operator {
-    void open();      // Get ready to work
-    String[] next();  // Give me the next row,kinda producing next row of output
-    void close();     // I'm done, clean up
-}
